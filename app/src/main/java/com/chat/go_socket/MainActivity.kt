e5d5cfb1fbbd8f5bdeaa61fa45651b9ff01a81de
Cjsghkd <pjhomg5530@naver.com>
2024-05-30 16:20:52 +0900

    :bug: hilt 코드 주석 처리

--- app/src/main/java/com/chat/go_socket/MainActivity.kt
+++ app/src/main/java/com/chat/go_socket/MainActivity.kt
@@ -3,14 +3,15 @@ package com.chat.go_socket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+//@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Text(text = "test")
         }
     }
 }
