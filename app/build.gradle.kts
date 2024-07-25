a6067e6674f0af33e18f268d8a50a573c92af720
sunghyun <pjhomg5530@naver.com>
2024-07-25 13:55:03 +0900

    :bug: :: automaticGenerationDuringBuild 로직 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
     alias(libs.plugins.baselineprofile)
 }
 
-android {
-    baselineProfile {
-        buildTypes {
-            getByName("release") {
-                automaticGenerationDuringBuild = true
-            }
-        }
-    }
-}
+//android {
+//    baselineProfile {
+//        buildTypes {
+//            getByName("release") {
+//                automaticGenerationDuringBuild = true
+//            }
+//        }
+//    }
+//}
 
 dependencies {
     implementation(libs.androidx.core.ktx)
@@ -24,4 +24,8 @@ dependencies {
 
     baselineProfile(project(":baselineprofile"))
     implementation(libs.androidx.profileinstaller)
+}
+
+baselineProfile {
+    automaticGenerationDuringBuild = true
 }
