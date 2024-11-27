39909a0d4d50f92c7b017d5ee46e7ec05e42c2ed
ImGaram <gim78014@gmail.com>
2024-11-27 20:32:14 +0900

    :lipstick: :: 없는 색 추가

--- core/designsystem/src/main/java/com/chat/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/chat/designsystem/theme/Color.kt
@@ -3,6 +3,28 @@ package com.chat.designsystem.theme
 import androidx.compose.ui.graphics.Color
 
 val P1 = Color(0xFF6263FB)
+val P2 = Color(0xFF9747FF)
+val P3 = Color(0xFFC0C0FF)
 
+val N1 = Color(0xFF0B0928)
+val N2 = Color(0xFF656B80)
 val N3 = Color(0xFFBBBBCC)
 val N4 = Color(0xFFE2E2EE)
+val N5 = Color(0xFFF2F2F4)
+
+val BG = Color(0xFFF8F9FB)
+val BG2 = Color(0xFFF2F2FC)
+val CARD = Color(0xFFFDFDFD)
+val ITEM_BG = Color(0xFFC1E7FE)
+val ITEM_BG2 = Color(0xFFEDF2FC)
+val ITEM_BG3 = Color(0xFFE5EDFE)
+
+val ERROR = Color(0xFFFF7D7D)
+val POSITIVE = Color(0xFF5EFF8B)
+val WARNING = Color(0xFFFCFF5E)
+
+val GREEN = Color(0xFF9BDA93)
+val YELLOW = Color(0xFFEEE170)
+val RED = Color(0xFFF3A199)
+val BLACK = Color(0xFF000000)
+val WHITE = Color(0xFFFFFFFF)
